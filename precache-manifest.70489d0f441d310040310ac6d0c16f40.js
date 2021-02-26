2d156cbd02489eb03bfad4bcb0f1af72a1ff69ea
ck <969211472@qq.com>
2021-02-26 22:02:37 +0800

    0.0.9

---
+++ precache-manifest.70489d0f441d310040310ac6d0c16f40.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d7be2be79cecc5e10136fe1cbf41d4",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/index.html"
+  },
+  {
+    "revision": "98a8abdd98f13b62398c",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/css/main.fd1d2709.chunk.css"
+  },
+  {
+    "revision": "463a1c70d938e7e5be41",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/0.3a2854c5.chunk.js"
+  },
+  {
+    "revision": "bb9cd51200254015d295",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/10.345fa92c.chunk.js"
+  },
+  {
+    "revision": "4d1368d1e75984f6408c",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/3.7494db4a.chunk.js"
+  },
+  {
+    "revision": "35d5344ccada58f363ecb1fca8ade690",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/3.7494db4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba0a7c3dac16ab5e232",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/4.cf6cdbf7.chunk.js"
+  },
+  {
+    "revision": "69f2fd39ba692cff6671",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/5.d5f1f678.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/5.d5f1f678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85bc18892fb4664e4c23",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/6.4806a66d.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/6.4806a66d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c0db1bb6b131fa2124",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/7.9a3a5228.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/7.9a3a5228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164a48ba027738950b74",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/8.71721fd3.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/8.71721fd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a7d95d83b15ad3c968",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/9.68ce5f26.chunk.js"
+  },
+  {
+    "revision": "98a8abdd98f13b62398c",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/main.2259066f.chunk.js"
+  },
+  {
+    "revision": "6fbf475c5ce994d8abf0",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/js/runtime-main.3877558d.js"
+  },
+  {
+    "revision": "17db7fc28ea8f9cc1b19f24c67b10c60",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/media/DAI.17db7fc2.png"
+  },
+  {
+    "revision": "c76b33ca42c5730ab77f3341ce9764a7",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/media/USDC.c76b33ca.png"
+  },
+  {
+    "revision": "839a0f0d6e0a69fed8d7846344d2100b",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/media/USDT.839a0f0d.png"
+  },
+  {
+    "revision": "240bfbe4edf288809812a2d3e3e29f9f",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/media/basis-bg.240bfbe4.jpg"
+  },
+  {
+    "revision": "294d995fef98309c889a1212b1833460",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/media/basis-bond-logo.294d995f.png"
+  },
+  {
+    "revision": "23e9f68d7b8ff82058ac09775b5edcfe",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/media/basis-cash-logo.23e9f68d.png"
+  },
+  {
+    "revision": "a565784444f8efc24a4d8e0b67675840",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/media/basis-cash-logo.a5657844.svg"
+  },
+  {
+    "revision": "c1f12e1dbae64c514584b4095dea9dcf",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/media/basis-share-logo.c1f12e1d.svg"
+  },
+  {
+    "revision": "fce3c34ea16d6da0a7d5ece1f2496adb",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/media/basis-share-logo.fce3c34e.png"
+  },
+  {
+    "revision": "f8fec857dc3b8e244e4c910c550b1df8",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/media/sUSD.f8fec857.png"
+  },
+  {
+    "revision": "88ae1ab3f40535ebbd248d03a7aab473",
+    "url": "https://cdn.jsdelivr.net/gh/ocashs/basisdappprd@0.0.9/static/media/ycrv.88ae1ab3.png"
+  }
+]);
